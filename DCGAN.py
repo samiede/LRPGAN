3154e1a6d3e4a34e1ecb7d82b022a79b5cd6b8da
Sami Ede <sami@node36.ml.tu-berlin.de>
2019-01-15 17:12:54 +0100

    Again, collapsed output

--- DCGAN.py
+++ DCGAN.py
@@ -291,7 +291,8 @@ for epoch in range(opt.epochs):
             print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
                   % (epoch, opt.epochs, n_batch, len(dataloader),
                      d_error_total.item(), g_err.item(), d_real, d_fake_1, d_fake_2))
-        print('var', add_noise_var)
+        
+            print('var', add_noise_var)
 
         if n_batch % 100 == 0:
             # generate fake with fixed noise
