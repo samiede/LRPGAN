d2cef116592b80734ed94130ad7857006e242b7c
Sami Ede <sami@node36.ml.tu-berlin.de>
2019-01-15 14:37:04 +0100

    Output for DCGAN.py with BN initialization around 0

--- DCGAN.py
+++ DCGAN.py
@@ -118,7 +118,7 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
+        m.weight.data.normal_(0.0, 0.02)
         m.bias.data.fill_(0)
 
 
