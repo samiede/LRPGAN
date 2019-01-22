0bcfbe25d5eab2aa00fc10c51052a597254712c8
Sami Ede <sami@node30.ml.tu-berlin.de>
2019-01-22 08:25:05 +0100

    Output for BN 1.0, alpha = 2, no negative relevance present

--- DCGAN.py
+++ DCGAN.py
@@ -119,7 +119,7 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(2.0, 0.02)
+        m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
 
