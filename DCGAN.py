1430b5372a9b50e341a4f9a586a94834b97cc33d
Sami Ede <sami.ede@icloud.com>
2019-01-24 17:00:08 +0100

    corrected first layer of generator and matching discriminator

--- DCGAN.py
+++ DCGAN.py
@@ -223,7 +223,7 @@ if opt.loadG != '':
     generator.load_state_dict(torch.load(opt.loadG))
 
 # discriminator = DiscriminatorNet(ngpu).to(gpu)
-discriminator = dcgm.DiscriminatorNet(nc, ndf, alpha, beta, ngpu).to(gpu)
+discriminator = dcgm.DiscriminatorNetVBN(nc, ndf, alpha, beta, ngpu).to(gpu)
 discriminator.apply(weights_init)
 if opt.loadD != '':
     discriminator.load_state_dict(torch.load(opt.loadG))
