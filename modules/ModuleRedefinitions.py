Fix to batch norm fuse calculation

modules/ModuleRedefinitions.py
@@ -216,9 +216,9 @@ class NextConvolution(nn.Conv2d):
 
         var_sqrt = torch.sqrt(var + eps)
 
-        w = (self.weight * gamma.reshape(self.out_channels, 1, 1, 1)) / var.reshape(self.out_channels, 1,
+        w = (self.weight * gamma.reshape(self.out_channels, 1, 1, 1)) / var_sqrt.reshape(self.out_channels, 1,
                                                                                                    1, 1)
-        b = ((self.bias - mean) * gamma) / var + beta
+        b = ((self.bias - mean) * gamma) / var_sqrt + beta
 
         self.weight = nn.Parameter(w)
         self.bias = nn.Parameter(b)
@@ -307,9 +307,9 @@ class NextConvolutionEps(nn.Conv2d):
 
         var_sqrt = torch.sqrt(var + eps)
 
-        w = (self.weight * gamma.reshape(self.out_channels, 1, 1, 1)) / var.reshape(self.out_channels, 1,
+        w = (self.weight * gamma.reshape(self.out_channels, 1, 1, 1)) / var_sqrt.reshape(self.out_channels, 1,
                                                                                                    1, 1)
-        b = ((self.bias - mean) * gamma) / var + beta
+        b = ((self.bias - mean) * gamma) / var_sqrt + beta
 
         self.weight = nn.Parameter(w)
         self.bias = nn.Parameter(b)